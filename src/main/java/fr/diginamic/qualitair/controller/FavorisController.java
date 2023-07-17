93bae8ae4688a51e7593cebdd26b38a362a7f61b
antoinecln56 <antoine.clain@hotmail.fr>
2023-07-17 16:28:09 +0200

    Get Post Delete Favoris OK

--- src/main/java/fr/diginamic/qualitair/controller/FavorisController.java
+++ src/main/java/fr/diginamic/qualitair/controller/FavorisController.java
@@ -1,9 +1,51 @@
 package fr.diginamic.qualitair.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import fr.diginamic.qualitair.dto.FavorisDto;
+import fr.diginamic.qualitair.dto.ForumUtilisateurDto;
+import fr.diginamic.qualitair.entites.Favoris;
+import fr.diginamic.qualitair.entites.Utilisateur;
+import fr.diginamic.qualitair.exception.NotFoundException;
+import fr.diginamic.qualitair.services.FavorisService;
+import fr.diginamic.qualitair.services.UtilisateurService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/favoris")
+@CrossOrigin
 public class FavorisController {
+
+    @Autowired
+    FavorisService favorisService;
+
+    @Autowired
+    UtilisateurService utilisateurService;
+
+    @GetMapping("/{id}")
+    public List<FavorisDto> listeFavorisByUser(@PathVariable Integer id){
+        return this.favorisService.getFavorisByUserId(id).stream().map(FavorisDto::from).collect(Collectors.toList());
+    }
+
+    @PostMapping()
+    public ResponseEntity<Favoris> ajoutFavoris(@RequestParam("idUtilisateur") Integer idUtilisateur, @RequestBody FavorisDto favorisDto) throws NotFoundException {
+        return ResponseEntity.ok(favorisService.ajoutFavoris(idUtilisateur, favorisDto));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> supprimerUtilisateur(@PathVariable(value = "id") Integer id) throws NotFoundException {
+        Optional<Favoris> suppressionFavoris = this.favorisService.findById(id);
+        if (suppressionFavoris.isPresent()) {
+            Favoris favorisSupprime = suppressionFavoris.get();
+            this.favorisService.delete(favorisSupprime);
+            return ResponseEntity.status(200).body("Le favori numéro " + id + " a été supprimé");
+        } else {
+            throw new NotFoundException();
+        }
+    }
+
 }
--- src/main/java/fr/diginamic/qualitair/dto/FavorisDto.java
+++ src/main/java/fr/diginamic/qualitair/dto/FavorisDto.java
@@ -1,10 +1,26 @@
 package fr.diginamic.qualitair.dto;
 
+import fr.diginamic.qualitair.entites.Favoris;
+import fr.diginamic.qualitair.entites.Utilisateur;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Service;
 
-@Service
+@Getter
+@Setter
 public class FavorisDto {
 
+    private String codePostal;
 
+    public FavorisDto() {
+
+    }
+    public FavorisDto(String codePostal, Integer idUtilisateur) {
+        this.codePostal = codePostal;
+    }
+
+    public static FavorisDto from(Favoris favoris) {
+        return new FavorisDto(favoris.getCodePostal(), favoris.getUtilisateur().getId());
+    }
 
 }
