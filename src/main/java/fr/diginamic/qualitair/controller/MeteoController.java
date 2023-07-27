f50bd45c6414fc1ebc76821ea2988468e47d92c3
antoinecln56 <antoine.clain@hotmail.fr>
2023-07-27 12:26:06 +0200

    Ajout CrossOrigin

--- src/main/java/fr/diginamic/qualitair/controller/MeteoController.java
+++ src/main/java/fr/diginamic/qualitair/controller/MeteoController.java
@@ -3,16 +3,14 @@ package fr.diginamic.qualitair.controller;
 import fr.diginamic.qualitair.entites.Meteo;
 import fr.diginamic.qualitair.services.WebApiService;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/meteo")
 public class MeteoController {
@@ -31,8 +29,8 @@ public class MeteoController {
      * @return la liste des enregistrements de météo entre les deux dates spécifiées en réponse HTTP
      */
     @GetMapping("/historique")
-    public List<Meteo> getHistoriqueMeteoEntreDeuxDates(@RequestParam("start") @DateTimeFormat(pattern = "dd/MM/yyyy") String start,
-                                                        @RequestParam("end") @DateTimeFormat(pattern = "dd/MM/yyyy") String end) {
+    public List<Meteo> getHistoriqueMeteo(@RequestParam("start") @DateTimeFormat(pattern = "dd/MM/yyyy") String start,
+                                          @RequestParam("end") @DateTimeFormat(pattern = "dd/MM/yyyy") String end) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         LocalDate startDate = LocalDate.parse(start, formatter);
         LocalDate endDate = LocalDate.parse(end, formatter);
