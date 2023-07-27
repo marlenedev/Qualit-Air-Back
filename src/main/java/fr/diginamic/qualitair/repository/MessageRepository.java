616bde45be491864a86e43b5ccb785bc10f69bde
Marlene AUBERT <aubert.marlene2@gmail.com>
2023-07-27 12:14:38 +0200

    feat/QUAL-gestion-messages

--- src/main/java/fr/diginamic/qualitair/repository/MessageRepository.java
+++ src/main/java/fr/diginamic/qualitair/repository/MessageRepository.java
@@ -1,9 +1,14 @@
 package fr.diginamic.qualitair.repository;
 
+import fr.diginamic.qualitair.entites.FilDiscussion;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import fr.diginamic.qualitair.entites.Message;
 
+import java.util.List;
+
 public interface MessageRepository extends JpaRepository<Message, Integer> {
+
+    List<Message> findAllByFilDiscussion (FilDiscussion filDiscussion);
 
 }
