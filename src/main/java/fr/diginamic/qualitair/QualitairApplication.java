91344e6881fe0b9eac08340236431b672b0a0aa6
antoinecln56 <antoine.clain@hotmail.fr>
2023-07-27 09:48:37 +0200

    Enregistrement de l'historique météo en base

--- src/main/java/fr/diginamic/qualitair/QualitairApplication.java
+++ src/main/java/fr/diginamic/qualitair/QualitairApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableScheduling //Active la planification des tâches
 public class QualitairApplication {
 
 	public static void main(String[] args) {
--- src/main/java/fr/diginamic/qualitair/entites/Meteo.java
+++ src/main/java/fr/diginamic/qualitair/entites/Meteo.java
@@ -1,20 +1,29 @@
 package fr.diginamic.qualitair.entites;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Meteo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    Long temp;
-    Long pressure;
-    Long humidity;
-    LocalDateTime date;
+    @Column(name = "code_postal")
+    private String codePostal;
+    private String temp;
+    private String pressure;
+    private String humidity;
+    private LocalDate date;
 }
